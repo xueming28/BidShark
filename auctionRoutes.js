b2c5d51330da63d7d51d46808f0670665fbe4351
01257066 <mikaelesmana27@gmail.com>
2025-10-18 13:51:52 +0800

    Delete auctionRoutes.js

--- auctionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Example route to test
-router.get("/", (req, res) => {
-  res.json({ message: "Auction route working!" });
-});
-
-module.exports = router;
