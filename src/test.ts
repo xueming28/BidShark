563f5964ed15ef755dce3112758c9aca3cfce839
Someone <linzhewei123@gmail.com>
2025-11-15 16:19:49 +0800

    cleaning

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@ import { connectDB } from "./ConnectToDB";
 async function main() {
     const db = await connectDB();         // wait for DB connection
     const users = db.collection("Users"); // get your collection
-    await users.insertOne({ email: "example@yahoo.com" });
+    await users.insertOne({ email: "example@yahoo.com",password: "123456" });
     const result = await users.find().toArray();
     console.log(result);
 }
