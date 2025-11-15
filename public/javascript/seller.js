7392fb4620f8896cdf43036daefe4c24df10fdda
Someone <linzhewei123@gmail.com>
2025-11-15 16:32:43 +0800

    cleaning

--- public/javascript/seller.js
+++ public/javascript/seller.js
@@ -1,3 +1,18 @@
+fetch('sideBar.html')
+    .then(res => res.text())
+    .then(html => {
+        const sidebar = document.getElementById('sidebar');
+        sidebar.innerHTML = html;
+        const links = sidebar.querySelectorAll('a.nav-item');
+        const currentPage = window.location.pathname.split('/').pop();
+        links.forEach(link => {
+            const linkPage = link.getAttribute('href');
+            if (linkPage === currentPage) {
+                link.classList.add('active');
+            }
+        });
+    });
+
 document.addEventListener('DOMContentLoaded', function() {
     
     const uploadForm = document.getElementById('itemUploadForm');
